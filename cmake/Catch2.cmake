f17f8eb9c936fb70722fc0fb1afd2453f2013f1b
Torsten Mehnert <92546601+tmehnert@users.noreply.github.com>
2021-10-21 18:13:05 +0200

    CMake target export (#9)
    * cmake: reformat files to more clarity
    
    * cmake: don't export code_coverage
    
    * cmake: export library targets
    
    * cmake: use PROJECT_SOURCE_DIR to enable cmake FetchContent
    
    * cmake: fix build with CODE_COVERAGE=on
    
    * cmake: targets should carry their public includes
    
    * cmake: consistent namespace and component name for the targets
    Users should be able to use the same string when refering the target,
    regardless the import method they used. (install vs fetch)
    
    * cmake: don't build unreferred targets on import
    When using FetchContent only targets which referred by the user
    should be build.
    
    * cmake: fix target name when imported
    
    * ci: workflow to test install via FetchContent
    
    * ci: use local dir to test FetchContent
    
    * ci: remove unused ctest switch
    
    * ci: workflow to test cmake find_package
    
    * ci: rename fetchcontent steps to reflect the test is build
    
    * docs: describe how to import the library using CMake

--- cmake/Catch2.cmake
+++ cmake/Catch2.cmake
@@ -14,11 +14,10 @@
 Include(FetchContent)
 
 message("-- Fetching Catch2...")
-FetchContent_Declare(
-        Catch2
-        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG        v2.13.7
-)
+FetchContent_Declare(Catch2
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+    GIT_TAG v2.13.7
+    )
 
 FetchContent_MakeAvailable(Catch2)
 list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
--- cmake/Trompeloeil.cmake
+++ cmake/Trompeloeil.cmake
@@ -14,11 +14,10 @@
 Include(FetchContent)
 
 message("-- Fetching Trompeloeil...")
-FetchContent_Declare(
-        Trompeloeil
-        GIT_REPOSITORY https://github.com/rollbear/trompeloeil.git
-        GIT_TAG        v41
-)
+FetchContent_Declare(Trompeloeil
+    GIT_REPOSITORY https://github.com/rollbear/trompeloeil.git
+    GIT_TAG v41
+    )
 
 FetchContent_MakeAvailable(Trompeloeil)
 message("-- Fetching Trompeloeil - done")
